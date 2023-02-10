6147000404e20a8f3a9f5a2501f0cf95685cedd4
OmerFarukTaştan <omerfaruk_tastan@hotmail.com>
2023-02-10 13:41:47 +0300

    Add project files.

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,54 @@
+﻿
+
+Console.Write("Enter number of cards to pick :");
+string line = Console.ReadLine();
+if (int.TryParse(line , out int numberOfCards) )
+{
+    foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+    {
+        Console.WriteLine(card);
+    }
+}
+else
+{
+    Console.WriteLine("Please enter a valid number");
+}
+class CardPicker
+{
+
+
+    static Random random = new Random();
+   public static string[] PickSomeCards(int numberOfCards)
+    {
+        string[] pickedCards = new string[numberOfCards];
+        for (int i = 0; i < numberOfCards; i++)
+        {
+            pickedCards[i] = RandomValue() + " of " + RandomSuit();
+        }
+        return pickedCards;
+    }
+
+    private static string RandomSuit()
+    {
+        int value = random.Next(1,5);
+        if (value == 1) return "Spades";
+        if (value == 2) return "Hearts";
+        if (value == 3) return "Clubs";
+        return "Diamonds";
+    }
+
+    private static string RandomValue()
+    {
+        int value = random.Next(1, 14);
+        if (value == 1) return "Ace";
+        if (value == 11) return "Jack";
+        if (value == 12) return "Queen";
+        if (value == 13) return "King";
+        return value.ToString();
+    }
+
+
+    
+
+}
+
